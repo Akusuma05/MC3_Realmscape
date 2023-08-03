4603f5f7c2b90397135f469719c556f785e0c523
Angelo Kusuma <84068505+Akusuma05@users.noreply.github.com>
2023-08-04 01:55:24 +0700

    Rotate and Translate is now available

--- MC3_Realmscape/Controller/ARViewController.swift
+++ MC3_Realmscape/Controller/ARViewController.swift
@@ -26,22 +26,48 @@ struct ARViewContainer: UIViewRepresentable {
         }
         
         arView.session.run(config)
-//        coffeeTable()
-//
-//        arView.enableObjectRemoval()
+        
+        arView.enableObjectRemoval()
         return arView
     }
     
     func updateUIView(_ uiView: ARView, context: Context) {
+        var isModelEntityPresent = false
+        
         if let model = modelConfirmedForPlacement {
             
-            if let modelEntity = model.modelEntity {
-                print("DEBUG: adding model to scene - \(model.modelName)")
+            if let modelEntity = model.modelEntity{
+                for anchor in uiView.scene.anchors {
+                    for entity in anchor.children {
+                        if entity == modelEntity {
+                            print("DEBUG: adding clone model to scene - \(model.modelName)")
+                            let clonedModelEntity = modelEntity.clone(recursive: true)
+                            let anchorEntity = AnchorEntity(plane: .any)
+                            uiView.scene.addAnchor(anchorEntity)
+                            anchorEntity.addChild(clonedModelEntity)
+                            
+                            clonedModelEntity.generateCollisionShapes(recursive: true)
+                            uiView.installGestures([.translation, .rotation], for: clonedModelEntity)
+                            isModelEntityPresent = true
+                            break
+                        }
+                        break
+                    }
+                    
+                }
+                if isModelEntityPresent == false{
+                    print("DEBUG: adding model to scene - \(model.modelName)")
+                    
+                    let anchorEntity = AnchorEntity(plane: .any)
+                    uiView.scene.addAnchor(anchorEntity)
+                    anchorEntity.addChild(modelEntity)
+                    
+                    //Move and rotate
+                    //Generate Collision Shapes
+                    modelEntity.generateCollisionShapes(recursive: true)
+                    uiView.installGestures([.translation, .rotation], for: modelEntity)
+                }
                 
-                let anchorEntity = AnchorEntity(plane: .any)
-                anchorEntity.addChild(modelEntity.clone(recursive: true))
-                
-                uiView.scene.addAnchor(anchorEntity)
             } else {
                 print("DEBUG: Unable to load modelEntity for \(model.modelName)")
             }
@@ -51,56 +77,26 @@ struct ARViewContainer: UIViewRepresentable {
             }
         }
     }
-    
-//    func createBox(){
-//        let mesh = MeshResource.generateBox(size: 0.2)
-//        let material = SimpleMaterial(color: .red, isMetallic: true)
-//        let modelEntity = ModelEntity(mesh: mesh, materials: [material])
-//        let anchorEntity = AnchorEntity(plane: .horizontal)
-//        anchorEntity.name = "CubeAnchor"
-//        anchorEntity.addChild(modelEntity)
-//        arView.scene.addAnchor(anchorEntity)
-//
-//        //Move and rotate
-//        //Generate Collision Shapes
-//        modelEntity.generateCollisionShapes(recursive: true)
-//
-//        //Install Gesture
-//        arView.installGestures([.translation, .rotation], for: modelEntity)
-//    }
-    
-//    func coffeeTable(){
-//        let entity = try! ModelEntity.loadModel(named: "robot_walk_idle")
-//        let anchor: AnchorEntity = AnchorEntity(plane: .horizontal)
-//        arView.scene.addAnchor(anchor)
-//        anchor.addChild(entity)
-//        //Move and rotate
-//        //Generate Collision Shapes
-//        entity.generateCollisionShapes(recursive: true)
-//
-//        //Install Gesture
-//        arView.installGestures([.translation, .rotation], for: entity)
-//    }
 }
 
-//extension ARView{
-//    //Delete Object
-//    func enableObjectRemoval() {
-//        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
-//        self.addGestureRecognizer(longPressGestureRecognizer)
-//    }
-//
-//    @objc func handleLongPress(recognizer: UILongPressGestureRecognizer){
-//        let location = recognizer.location(in: self)
-//
-//        if let entity = self.entity(at: location){
-//            if let anchorEntity = entity.anchor, anchorEntity.name == "CubeAnchor"{
-//                anchorEntity.removeFromParent()
-//                print("Removed anchor with name: " + anchorEntity.name)
-//            }
-//        }
-//    }
-//}
+extension ARView{
+    //Delete Object
+    func enableObjectRemoval() {
+        let longPressGestureRecognizer = UILongPressGestureRecognizer(target: self, action: #selector(handleLongPress(recognizer:)))
+        self.addGestureRecognizer(longPressGestureRecognizer)
+    }
+    
+    @objc func handleLongPress(recognizer: UILongPressGestureRecognizer){
+        let location = recognizer.location(in: self)
+        
+        if let entity = self.entity(at: location){
+            if let anchorEntity = entity.anchor, anchorEntity.name == "CubeAnchor"{
+                anchorEntity.removeFromParent()
+                print("Removed anchor with name: " + anchorEntity.name)
+            }
+        }
+    }
+}
 
 
 
